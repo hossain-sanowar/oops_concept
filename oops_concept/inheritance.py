3ea6600ac2bdc0de717816edd87d10238de18e9c
hossain-sanowar <md.sanowar21@gmail.com>
2025-12-08 23:25:19 +0100

    updated inheritance

--- oops_concept/inheritance.py
+++ oops_concept/inheritance.py
@@ -9,6 +9,21 @@ class Student:
     def get_name(self):
         return self.name
 
+class HR(Student):
+    def __init__(self, name, age, total_hour):
+        super().__init__(name, age)
+        self.name=name
+        self.total_hour=total_hour
+        self.total_salary=10
+
+    def get_total_salary(self):
+        return self.total_salary*self.total_hour
+
+
+
 stu=Student("John", 25)
 print(stu.get_age())
 print(stu.get_name())
+
+hr=HR("John", 25)
+print(hr.get_total_salary())
