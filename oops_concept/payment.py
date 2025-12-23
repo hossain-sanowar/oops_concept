2e1bba2ddede13efb94d4ce083fd7a01251b63b9
hossain-sanowar <md.sanowar21@gmail.com>
2025-12-23 20:39:23 +0100

    add validate method

--- oops_concept/payment.py
+++ oops_concept/payment.py
@@ -3,6 +3,14 @@ from abc import ABC, abstractmethod
 
 class Payment(ABC):
     @abstractmethod
-    def process_payment(self, amount):
+    def process_payment(self):
         pass
+
+# define concreate one class
+class CreditCard(Payment):
+    def Validate_payment(self, amount):
+        if amount < 0:
+            return False
+        return True
+
 
