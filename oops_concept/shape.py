f3c25e17e1a0f414d6a4da826f0f6613b3386015
hossain-sanowar <md.sanowar21@gmail.com>
2025-12-23 21:58:47 +0100

    add method perimeter rectangle class

--- oops_concept/shape.py
+++ oops_concept/shape.py
@@ -22,5 +22,9 @@ class Rectangle(Shape):
     def area(self, height, width):
         return height*width
 
+    def perimeter(self, height, width):
+        return 2*(height+width)
+
 rectangle=Rectangle()
 print(rectangle.area(10,10))
+print(rectangle.perimeter(10,10))
