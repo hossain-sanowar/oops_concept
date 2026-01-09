190ff2ebf21e0463e1ef6734549ad5146258952a
hossain-sanowar <md.sanowar21@gmail.com>
2026-01-09 23:20:09 +0100

    add flask app

--- oops_concept/web_app.py
+++ oops_concept/web_app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-@app.route("/hello")
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
 
+@app.route('/hello')
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__=='__main__':
+    app.run(debug=True)
