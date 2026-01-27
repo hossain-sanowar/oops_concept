c4ef9eed79ecd6a0fdacb807cdd52de9b6ab19d3
hossain-sanowar <md.sanowar21@gmail.com>
2026-01-27 23:44:59 +0100

    add list read method2

--- oops_concept/dict_class.py
+++ oops_concept/dict_class.py
@@ -20,5 +20,13 @@ class listRead:
             data_store.append(item)
         return data_store
 
+    def read_list2(self):
+        data_store=[]
+        for item in self.data:
+            if item%2==0:
+                data_store.append(item)
+        return data_store
+
 listClass=listRead([1,3,4,5,6,8])
 print(listClass.read_list())
+print(listClass.read_list2())
