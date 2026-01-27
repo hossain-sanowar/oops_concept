2bdc01dedbe783ce7076f6575451218789949eb6
hossain-sanowar <md.sanowar21@gmail.com>
2026-01-27 23:31:14 +0100

    add getattr method

--- oops_concept/dict_class.py
+++ oops_concept/dict_class.py
@@ -2,5 +2,8 @@ class DictClass:
     def __init__(self, **kwargs):
         self.data = kwargs
 
+    def __getattr__(self, item):
+        return self.data[item]
+
 dictclass=DictClass()
 print(dictclass.data)
