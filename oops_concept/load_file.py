55733c5a099721ff4fc669039e64c54d31ecfb3b
hossain-sanowar <md.sanowar21@gmail.com>
2026-01-28 23:29:43 +0100

    add listWrite class

--- oops_concept/load_file.py
+++ oops_concept/load_file.py
@@ -2,4 +2,9 @@ from dict_class import listRead
 from oops_concept.dict_class import listRead
 
 listclass=listRead([3,4,5,6,8])
-print(listclass.read_list2())
+#print(listclass.read_list2())
+
+class listWrite:
+    def __init__(self, data):
+
+
