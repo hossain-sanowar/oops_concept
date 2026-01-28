01f3d4913d0daac6fef52b63ce3fbf657b7cb621
hossain-sanowar <md.sanowar21@gmail.com>
2026-01-28 23:26:34 +0100

    add read file2

--- oops_concept/load_file.py
+++ oops_concept/load_file.py
@@ -1,5 +1,5 @@
 from dict_class import listRead
-from oops_concept.dict_class import listClass
+from oops_concept.dict_class import listRead
 
-listclass=listClass([3,4,5,6,8])
-print(listclass.read_list())
+listclass=listRead([3,4,5,6,8])
+print(listclass.read_list2())
