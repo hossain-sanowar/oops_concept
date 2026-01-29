75348f249562055e03f95d82626ef96fcf6f3532
hossain-sanowar <md.sanowar21@gmail.com>
2026-01-29 23:16:51 +0100

    add even method

--- oops_concept/data_read.py
+++ oops_concept/data_read.py
@@ -5,6 +5,13 @@ class lsitItem:
     def read(self):
         return self.data
 
+    def get_even(self):
+        even = []
+        for item in self.data:
+            if item % 2 == 0:
+                even.append(item)
+        return even
+
 
 clslist = lsitItem([1,2,3,4,5])
 print(clslist.read())
