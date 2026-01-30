8dff784fe75c0b2faafcb7995f6640d3fc99a04f
hossain-sanowar <md.sanowar21@gmail.com>
2026-01-30 22:57:07 +0100

    test list class

--- oops_concept/list_data.py
+++ oops_concept/list_data.py
@@ -1,9 +1,12 @@
 class list_data:
-    def __init__(self):
-        self.data = []
+    def __init__(self,data):
+        self.data = data
 
     def get_even(self):
         even = []
         for item in self.data:
             even.append(item)
+        return even
 
+clslist=list_data([1,2,3,4,5,6])
+print(clslist.get_even())
