1c482ba921f6d79a060677f80cde5d80efe0dc68
hossain-sanowar <md.sanowar21@gmail.com>
2026-01-31 23:17:03 +0100

    test class and method

--- oops_concept/numpy_class.py
+++ oops_concept/numpy_class.py
@@ -2,7 +2,10 @@ class Shape:
     def __init__(self,radius):
         self.radius=radius
 
-    def area(self,shape):
+    def area(self):
         pi=3.14
-        return pi*shape.radius**2
+        return pi*self.radius**2
 
+
+clsshape=Shape(10)
+print(clsshape.area())
