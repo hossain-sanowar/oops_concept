09e97f978a23eacdc901aad0788f4ac9f0793291
hossain-sanowar <md.sanowar21@gmail.com>
2026-02-03 23:20:38 +0100

    add read_list method

--- oops_concept/data_list.py
+++ oops_concept/data_list.py
@@ -1,3 +1,9 @@
 class List:
     def __init__(self,value):
         self.value=value
+
+    def read_list(self):
+        self.list=[]
+        for item in self.value:
+            self.list.append(item)
+        return self.list
