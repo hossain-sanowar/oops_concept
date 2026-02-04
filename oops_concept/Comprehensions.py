137cc616f772bff0e5770309c99892b40fe6e10c
hossain-sanowar <md.sanowar21@gmail.com>
2026-02-04 23:51:10 +0100

    numpy array

--- oops_concept/Comprehensions.py
+++ oops_concept/Comprehensions.py
@@ -12,7 +12,12 @@ print(lst)
 gen =(x*x for x in range(10**6))
 print(gen)'''
 
-results=[]
+'''results=[]
 for x in range(1_000_000):
     results.append(x*x )
+print(results)'''
+
+import numpy as np
+arr =np.arange(10)
+results=arr*arr
 print(results)
