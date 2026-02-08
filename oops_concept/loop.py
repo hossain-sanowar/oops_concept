64bd42c914b9651920c71aab20766413a08dc558
hossain-sanowar <md.sanowar21@gmail.com>
2026-02-08 23:27:02 +0100

    get index and value

--- oops_concept/loop.py
+++ oops_concept/loop.py
@@ -1,5 +1,12 @@
 # for loop
 data=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+'''
 for x in data:
     if x%2==0:
-        print(x)
+        print(x)'''
+
+'''for data in range(1,11):
+    print(data)'''
+
+for index, value in enumerate(data):
+    print(index, value)
