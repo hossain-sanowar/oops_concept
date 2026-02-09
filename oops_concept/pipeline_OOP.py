3a447c2ca8482d37af20ee3034090e75a84c0763
hossain-sanowar <md.sanowar21@gmail.com>
2026-02-09 23:08:47 +0100

    add transform

--- oops_concept/pipeline_OOP.py
+++ oops_concept/pipeline_OOP.py
@@ -4,3 +4,5 @@ class StandardScalerStep:
         self.var = sum((x - self.mean) ** 2 for x in data) / len(data)
         self.std = self.var ** 0.5 if self.var > 0 else 1.0
         return self
+
+    def transform(self, data): return [(x - self.mean) / self.std for x in data]
