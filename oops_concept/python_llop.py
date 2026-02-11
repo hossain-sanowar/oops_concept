d87f5a301b779e276055c94bb26c9e34edaf814e
hossain-sanowar <md.sanowar21@gmail.com>
2026-02-11 23:27:39 +0100

    add total method

--- oops_concept/python_llop.py
+++ oops_concept/python_llop.py
@@ -6,6 +6,8 @@ class Loop:
         for i in self.data:
             yield i
 
+    def total(self):
+        return sum(self.data())
 
 clsLoop=Loop(data=[1,2,3,4,5])
 iterate_data=clsLoop.iterate()
