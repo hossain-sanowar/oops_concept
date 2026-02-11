187726107ed4783f8499b1e014efefa30818217b
hossain-sanowar <md.sanowar21@gmail.com>
2026-02-11 23:30:56 +0100

    add even

--- oops_concept/python_llop.py
+++ oops_concept/python_llop.py
@@ -9,6 +9,10 @@ class Loop:
     def total(self,a):
         return sum(a)
 
+    def even(self,a):
+        if a%2 == 0:
+            return True
+
 clsLoop=Loop(data=[1,2,3,4,5])
 iterate_data=clsLoop.iterate()
 print(list(iterate_data))
