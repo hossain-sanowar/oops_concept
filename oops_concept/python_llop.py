6b67f2efe3a867247c69a2140739dd0f51563b14
hossain-sanowar <md.sanowar21@gmail.com>
2026-02-11 23:30:03 +0100

    test single return

--- oops_concept/python_llop.py
+++ oops_concept/python_llop.py
@@ -6,9 +6,11 @@ class Loop:
         for i in self.data:
             yield i
 
-    def total(self):
-        return sum(self.data())
+    def total(self,a):
+        return sum(a)
 
 clsLoop=Loop(data=[1,2,3,4,5])
 iterate_data=clsLoop.iterate()
 print(list(iterate_data))
+
+print(clsLoop.total([1,2,3,4,5]))
