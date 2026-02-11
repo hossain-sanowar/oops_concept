3353ab41caa19a5409a4c040ee3ad34ee646ec9d
hossain-sanowar <md.sanowar21@gmail.com>
2026-02-11 23:32:45 +0100

    add even test

--- oops_concept/python_llop.py
+++ oops_concept/python_llop.py
@@ -10,11 +10,17 @@ class Loop:
         return sum(a)
 
     def even(self,a):
-        if a%2 == 0:
-            return True
+        even_data=[]
+        for i in a:
+            if i % 2 == 0:
+                even_data.append(i)
+        return even_data
+
+
 
 clsLoop=Loop(data=[1,2,3,4,5])
 iterate_data=clsLoop.iterate()
 print(list(iterate_data))
 
 print(clsLoop.total([1,2,3,4,5]))
+print(clsLoop.even([1,2,3,4,5]))
