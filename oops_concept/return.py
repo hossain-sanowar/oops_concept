9fb47f3fb7a58c1ed72bbe5956fc2c5bf8a01523
hossain-sanowar <md.sanowar21@gmail.com>
2026-02-13 22:39:09 +0100

    add total

--- oops_concept/return.py
+++ oops_concept/return.py
@@ -1,3 +1,17 @@
 class Return:
     def __init__(self, value):
         self.value = value
+
+    def total(self, a):
+        Total=sum(a)
+        return Total
+
+
+
+
+
+
+
+clsReturn = Return(5)
+print(clsReturn)
+print(clsReturn.total([1,2,3]))
