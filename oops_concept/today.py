f457f78d1e13cca8f0e89d577dc83bff9869697e
hossain-sanowar <md.sanowar21@gmail.com>
2026-02-16 23:42:25 +0100

    add odd

--- oops_concept/today.py
+++ oops_concept/today.py
@@ -8,6 +8,13 @@ class List:
             data.append(item)
         return data
 
+    def _get_odd(self):
+        odd=[]
+        for item in self.data:
+            if item % 2 != 0:
+                odd.append(item)
+        return odd
+
 
 clsList = List([1,2,3,4,5])
 print(clsList)
