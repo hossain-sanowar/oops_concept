246ea15faec28ed86054d62459a313a324a77278
hossain-sanowar <md.sanowar21@gmail.com>
2026-02-16 23:40:33 +0100

    add read list method

--- oops_concept/today.py
+++ oops_concept/today.py
@@ -2,6 +2,12 @@ class List:
     def __init__(self, data):
         self.data = data
 
+    def _read_list(self):
+        data = []
+        for item in self.data:
+            data.append(item)
+        return data
+
 
 clsList = List([1,2,3,4,5])
 print(clsList)
