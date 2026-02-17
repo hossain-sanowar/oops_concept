34be31cec14f9a86829fefa9c9ae19f023fa2f0e
hossain-sanowar <md.sanowar21@gmail.com>
2026-02-17 23:01:02 +0100

    add number

--- oops_concept/today.py
+++ oops_concept/today.py
@@ -29,6 +29,13 @@ class List:
 
         return index
 
+    def _get_add(self):
+        add=[]
+        for item in self.data:
+            add.append(item)
+        return add
+
+
 
 
 clsList = List([1,2,3,4,5])
