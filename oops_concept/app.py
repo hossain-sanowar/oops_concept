0a497bd458b23864d068075cba538581c0fc0af6
hossain-sanowar <md.sanowar21@gmail.com>
2026-02-17 23:09:32 +0100

    data

--- oops_concept/app.py
+++ oops_concept/app.py
@@ -1,3 +1,13 @@
 class App:
     def __init__(self, data):
         self.data = []
+
+    def _get_add(self):
+        data=[]
+        for item in self.data:
+            data.append(item)
+
+        return data
+
+
+clsApp = App([1,2,3,4,5])
